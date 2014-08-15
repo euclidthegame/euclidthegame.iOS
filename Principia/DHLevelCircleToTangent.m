e7ee796c9c385b51f96c56351827b2ad5c6ae7d0
David Hallgren <dhallgren@gmail.com>
2014-08-15 06:04:23 +0200

    Fix to level 18 hint

--- Principia/DHLevelCircleToTangent.m
+++ Principia/DHLevelCircleToTangent.m
@@ -9,6 +9,7 @@
 #import "DHLevelCircleToTangent.h"
 
 #import "DHGeometricObjects.h"
+#import "DHLevelViewController.h"
 
 @interface DHLevelCircleToTangent () {
     DHLine* _givenLine;
@@ -129,8 +130,7 @@
 
 - (BOOL)isLevelCompleteHelper:(NSMutableArray*)geometricObjects
 {
-    for (int index = 0; index < geometricObjects.count; ++index) {
-        id object = [geometricObjects objectAtIndex:index];
+    for (id object in geometricObjects) {
         if ([[object class]  isSubclassOfClass:[DHCircle class]] == NO) continue;
         
         DHCircle* circle = object;
@@ -178,31 +178,27 @@
     return CGPointMake(NAN, NAN);
 }
 
--(void)hint:(NSMutableArray *)geometricObjects and:(UISegmentedControl *)toolControl and:(UILabel *)toolInstructions and:(DHGeometryView *)geometryView and:(UIView *)view and:(NSLayoutConstraint *)heightToolBar and:(UIButton *)hintButton {
+- (void)showHint
+{
+    DHGeometryView* geometryView = self.levelViewController.geometryView;
     
-    if ([self.hintButton.titleLabel.text isEqualToString:@"Hide hint"] ) {
+    if (self.showingHint) {
         [self hideHint];
         return;
     }
     
-    if (hint1_OK && hint2_OK) {
-        [self showTemporaryMessage:@"No more hints available." atPoint:CGPointMake(self.geometryView.center.x,50) withColor:[UIColor darkGrayColor] andTime:3.0];
-        [hintButton setTitle:@"Show hint" forState:UIControlStateNormal];
+    self.showingHint = YES;
+    
+    [self slideOutToolbar];
+    
+    if (hint2_OK) {
         hint1_OK = NO;
         hint2_OK = NO;
-        return;
     }
     
-    [hintButton setTitle:@"Hide hint" forState:UIControlStateNormal];
-    for (int a=0; a<90; a++) {
-        [self performBlock:^{
-            heightToolBar.constant= 70 - a;
-        } afterDelay:a* (1/90.0) ];
-    }
-    
-    Message* message1 = [[Message alloc] initWithMessage:@"Which properties does a circle have if it is tangent to a line ? " andPoint:CGPointMake(30,400)];
-    Message* message2 = [[Message alloc] initWithMessage:@"What do we know about the line connecting the center with the tangent point ?" andPoint:CGPointMake(30,420)];
-    Message* message3 = [[Message alloc] initWithMessage:@"It is an interseting fact, we've learned in Level 13." andPoint:CGPointMake(30,440)];
+    Message* message1 = [[Message alloc] initWithMessage:@"Which properties does a circle have if it is tangent to a line? " andPoint:CGPointMake(30,400)];
+    Message* message2 = [[Message alloc] initWithMessage:@"What do we know about the line connecting the center with the tangent point?" andPoint:CGPointMake(30,420)];
+    Message* message3 = [[Message alloc] initWithMessage:@"It is an interseting fact, we learned in Level 13." andPoint:CGPointMake(30,440)];
     Message* message4 = [[Message alloc] initWithMessage:@"The perpendicular bisector of DE must pass through the center." andPoint:CGPointMake(30,460)];
     
     DHPerpendicularLine* pl1 = [[DHPerpendicularLine alloc] init];
@@ -246,74 +242,71 @@
         [message4 position: CGPointMake(150,560)];
     }
     
-    UIView* hintView = [[UIView alloc]initWithFrame:geometryView.frame];
-    [geometryView addSubview:hintView];
-    [hintView addSubview:bisectorView];
-    [hintView addSubview:circleView];
-    [hintView addSubview:perpView];
-    [hintView addSubview:message1];
-    [hintView addSubview:message2];
-    [hintView addSubview:message3];
-    [hintView addSubview:message4];
-
-    
-    if (!hint1_OK) {
-        [self afterDelay:0.0 performBlock:^{
-            [self fadeIn:message1 withDuration:1.0];
-            [self fadeIn:circleView withDuration:1.0];
-        }];
-        [self afterDelay:4.0 performBlock:^{
-            [self fadeIn:message2 withDuration:1.0];
-        }];
-        [self afterDelay:8.0 performBlock:^{
-            [self fadeIn:message3 withDuration:1.0];
-            
-            [self fadeIn:perpView withDuration:2.0];
-                        hint1_OK = YES;
-
-        }];
-
-    }
-    else if (!hint2_OK) {
+    [self afterDelay:1.0 :^{
+        if (!self.showingHint) return;
         
-        [self afterDelay:0.0 performBlock:^{
-            [message1 text:@"We know that the circle must pass through A and B."];
-            [self fadeIn:message1 withDuration:1.0];
-            [self fadeIn:circleView withDuration:1.0];
+        
+        UIView* hintView = [[UIView alloc]initWithFrame:geometryView.frame];
+        [geometryView addSubview:hintView];
+        [hintView addSubview:bisectorView];
+        [hintView addSubview:circleView];
+        [hintView addSubview:perpView];
+        [hintView addSubview:message1];
+        [hintView addSubview:message2];
+        [hintView addSubview:message3];
+        [hintView addSubview:message4];
+        
+        [self afterDelay:0.5 :^{
+            [self showEndHintMessageInView:hintView];
         }];
-        [self afterDelay:4.0 performBlock:^{
-            [message2 text:@"So A and B are equidistant from the center."];
-            [self fadeIn:message2 withDuration:1.0];
-            [perpView.geometricObjects addObject:segment2];
-            [perpView setNeedsDisplay];
-            [self fadeIn:perpView withDuration:2.0];
+        
+        
+        if (!hint1_OK) {
+            [self afterDelay:0.0 performBlock:^{
+                [self fadeIn:message1 withDuration:1.0];
+                [self fadeIn:circleView withDuration:1.0];
+            }];
+            [self afterDelay:4.0 performBlock:^{
+                [self fadeIn:message2 withDuration:1.0];
+            }];
+            [self afterDelay:8.0 performBlock:^{
+                [self fadeIn:message3 withDuration:1.0];
+                [self fadeIn:perpView withDuration:2.0];
+                
+                hint1_OK = YES;
+            }];
             
-        }];
-        [self afterDelay:8.0 performBlock:^{
-            [message3 text:@"What do we know about the perpendicular bisector of line segment AB?"];
-            [self fadeIn:message3 withDuration:1.0];
+        }
+        else if (!hint2_OK) {
             
-        }];
-        [self afterDelay:12.0 performBlock:^{
-            [message4 text:@"It is an interseting fact we've learned in Level 14."];
-            [self fadeIn:message4 withDuration:1.0];
-            [self fadeIn:bisectorView withDuration:2.0];
-            
-            
-            hint2_OK = YES;
-        }];
-    }
+            [self afterDelay:0.0 performBlock:^{
+                [message1 text:@"We know that the circle must pass through A and B."];
+                [self fadeIn:message1 withDuration:1.0];
+                [self fadeIn:circleView withDuration:1.0];
+            }];
+            [self afterDelay:4.0 performBlock:^{
+                [message2 text:@"So A and B are equidistant from the center."];
+                [self fadeIn:message2 withDuration:1.0];
+                [perpView.geometricObjects addObject:segment2];
+                [perpView setNeedsDisplay];
+                [self fadeIn:perpView withDuration:2.0];
+                
+            }];
+            [self afterDelay:8.0 performBlock:^{
+                [message3 text:@"What do we know about the perpendicular bisector of line segment AB?"];
+                [self fadeIn:message3 withDuration:1.0];
+                
+            }];
+            [self afterDelay:12.0 performBlock:^{
+                [message4 text:@"It is an interseting fact we learned in Level 14."];
+                [self fadeIn:message4 withDuration:1.0];
+                [self fadeIn:bisectorView withDuration:2.0];
+                
+                hint2_OK = YES;
+            }];
+        }
+    }];
     
 }
--(void)hideHint {
-    for (int a=0; a<90; a++) {
-        [self performBlock:^{
-            self.heightToolbar.constant= -20 + a;
-        } afterDelay:a* (1/90.0) ];
-    }
-    if (!hint1_OK){        [self.hintButton setTitle:@"Show hint" forState:UIControlStateNormal];}
-    else {[self.hintButton setTitle:@"Show next hint" forState:UIControlStateNormal];}
-    [self.geometryView.subviews makeObjectsPerformSelector: @selector(removeFromSuperview)];
-    return;
-}
+
 @end
